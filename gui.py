5cb66228a277d24edba0e6e27ff66d4a50238587
Wout Rombouts <woutrombouts18@gmail.com>
2023-03-29 18:35:22 +0200

    Reorganised images and improved GUI

--- gui.py
+++ gui.py
@@ -4,66 +4,90 @@ and the three ways to process button events.
 """
 import arcade
 import arcade.gui
+from arcade.experimental.uislider import UISlider
+from arcade.gui import UIManager, UIAnchorWidget, UILabel
+from arcade.gui.events import UIOnChangeEvent
+from pyglet.image import load as pyglet_load
 
-
-# --- Method 1 for handling click events,
-# Create a child class.
-class QuitButton(arcade.gui.UIFlatButton):
-    def on_click(self, event: arcade.gui.UIOnClickEvent):
-        arcade.exit()
-
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 
 class MyWindow(arcade.Window):
     def __init__(self):
-        super().__init__(800, 600, "Unstable Unicorns", resizable=True)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Unstable Unicorns", resizable=True)
 
-        # --- Required for all code that uses UI element,
-        # a UIManager to handle the UI.
         self.manager = arcade.gui.UIManager()
         self.manager.enable()
 
-        # Set background color
         arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+        
+        self.set_icon(pyglet_load("images/icon.png"))
+        self.background = arcade.load_texture("images/background.jpg")
 
-        # Create a vertical BoxGroup to align buttons
         self.v_box = arcade.gui.UIBoxLayout()
+        
+        default_style = {
+            "font_name": ("calibri", "arial"),
+            "font_size": 15,
+            "font_color": arcade.color.WHITE,
+            "border_width": 2,
+            "border_color": None,
+            "bg_color": arcade.color.AMETHYST,
 
-        # Create the buttons
-        start_button = arcade.gui.UIFlatButton(text="Spel starten", width=200)
+            "bg_color_pressed": arcade.color.BRIGHT_LILAC,
+            "border_color_pressed": arcade.color.WHITE,
+            "font_color_pressed": arcade.color.BLACK,
+        }
+        
+        ui_text_label = arcade.gui.UITextArea(
+            text="Unstable Unicorns",
+            width=568,
+            height=60,
+            font_size=32,
+            font_name="Kenney Future",
+            text_color=arcade.color.AMETHYST
+        )
+        self.v_box.add(ui_text_label.with_space_around(bottom=60))
+
+        start_button = arcade.gui.UIFlatButton(text="Spel starten", width=200, style=default_style)
         self.v_box.add(start_button.with_space_around(bottom=20))
 
-        settings_button = arcade.gui.UIFlatButton(text="Instellingen", width=200)
+        settings_button = arcade.gui.UIFlatButton(text="Instellingen", width=200, style=default_style)
         self.v_box.add(settings_button.with_space_around(bottom=20))
+        
+        exit_button = arcade.gui.UIFlatButton(text="Afsluiten", width=200, style=default_style)
+        self.v_box.add(exit_button.with_space_around(bottom=20))
 
-        # Again, method 1. Use a child class to handle events.
-        quit_button = QuitButton(text="Afsluiten", width=200)
-        self.v_box.add(quit_button)
-
-        # --- Method 2 for handling click events,
-        # assign self.on_click_start as callback
-        start_button.on_click = self.on_click_start
-
-        # --- Method 3 for handling click events,
-        # use a decorator to handle on_click events
+        @start_button.event("on_click")
+        def on_click_settings(event):
+            print("Start:", event)
+            
         @settings_button.event("on_click")
         def on_click_settings(event):
             print("Settings:", event)
+            
+        @exit_button.event("on_click")
+        def on_click_settings(event):
+            arcade.exit()
 
-        # Create a widget to hold the v_box widget, that will center the buttons
         self.manager.add(
             arcade.gui.UIAnchorWidget(
                 anchor_x="center_x",
                 anchor_y="center_y",
                 child=self.v_box)
         )
-
-    def on_click_start(self, event):
-        print("Start:", event)
-
+        
+    def setup(self):
+        self.music = arcade.Sound("sounds/soundtrack.mp3", streaming=True)
+        self.current_player = self.music.play(0.5)
+        
     def on_draw(self):
         self.clear()
+        
+        arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         self.manager.draw()
 
 
 window = MyWindow()
+window.setup()
 arcade.run()
